722d5221f51ef31c7006425bbb039bc5b5f2b7e6
Jonathan Levi <jonathan1996levi@gmail.com>
2021-10-02 13:46:09 +0100

    [Phantom] Turn off wallet connection, for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,44 @@
 // HMJ
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-import { PhantomProvider, SolanaWindow } from './typings';
+// import { PhantomProvider, SolanaWindow } from './typings';
 
-const getProvider = (): PhantomProvider | undefined => {
-  const anyWindow: SolanaWindow = window as SolanaWindow;
-  const provider = anyWindow.solana;
+// const getProvider = (): PhantomProvider | undefined => {
+//   const anyWindow: SolanaWindow = window as SolanaWindow;
+//   const provider = anyWindow.solana;
 
-  if (provider != null && provider.isPhantom) {
-    return provider;
-  }
+//   if (provider != null && provider.isPhantom) {
+//     return provider;
+//   }
 
-  window.open('https://phantom.app/', '_blank');
-};
+//   window.open('https://phantom.app/', '_blank');
+// };
+
+// TODO: mint NFT
+// TODO: add it to your wallet
 
 const App = (): React.ReactElement => {
-  const provider = getProvider();
-  const [, setConnected] = useState(false);
-  useEffect(() => {
-    if (provider) {
-      provider.on('connect', () => {
-        setConnected(true);
-      });
-      provider.on('disconnect', () => {
-        setConnected(false);
-      });
-      provider.connect();
+  // const provider = getProvider();
+  // const [, setConnected] = useState(false);
+  // useEffect(() => {
+  //   if (provider) {
+  //     provider.on('connect', () => {
+  //       setConnected(true);
+  //     });
+  //     provider.on('disconnect', () => {
+  //       setConnected(false);
+  //     });
+  //     provider.connect();
 
-      return () => {
-        provider.disconnect();
-      };
-    }
-  }, [provider]);
+  //     return () => {
+  //       provider.disconnect();
+  //     };
+  //   }
+  // }, [provider]);
+
+  // https://stackoverflow.com/questions/68215033/i-would-like-to-mint-a-new-token-on-solana-how-can-i-do-this-using-solana-web3
+  // const createNFT = async () => {
+  // }
 
   return (
     <div className="App">
