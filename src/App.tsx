4be1ff4dd1f193b8ae614c6de00a998ba4713fe8
Jonathan Levi <jonathan1996levi@gmail.com>
2021-10-02 16:25:39 +0100

    [Bear] adjust CSS to 100vh

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,13 @@ import SplineTest from './Spline-test';
 const App = (): React.ReactElement => {
   return (
     <div className="App">
-      <div className="w-full h-96">
+      <div className="w-full h-screen">
         <Suspense fallback="Loading">
           <Canvas>
             <MinBear />
             <SplineTest />
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            {/* <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} /> */}
           </Canvas>
         </Suspense>
       </div>
